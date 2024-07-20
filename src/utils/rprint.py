2e7de2e3975e872dcfc5ac91d2e514e60b4bcf44
basitatube <165612250+basitatube@users.noreply.github.com>
2024-07-20 05:56:47 +0300

    Update rprint.py

--- src/utils/rprint.py
+++ src/utils/rprint.py
@@ -7,8 +7,10 @@ custom print and log functions
 __all__ = ['rprint', 'rlog']
 
 def rprint(*args, **kwargs):
+    kwargs.pop('style', None)  # Remove 'style' if it is present
     print(*args, **kwargs)
 
 def rlog(*args, **kwargs):
+    kwargs.pop('style', None)  # Remove 'style' if it is present
     print(*args, **kwargs)
 
