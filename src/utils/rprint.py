2ae48498252b31f08ca3de13ced4b667fbc4c9b6
basitatube <165612250+basitatube@users.noreply.github.com>
2024-07-20 05:39:43 +0300

    Update rprint.py

--- src/utils/rprint.py
+++ src/utils/rprint.py
@@ -6,11 +6,9 @@ custom print and log functions
 
 __all__ = ['rprint', 'rlog']
 
-try:
-    from rich.console import Console
-    console = Console()
-    rprint = console.print
-    rlog = console.log
-except:
-    rprint = print
-    rlog = print
+def rprint(*args, **kwargs):
+    print(*args, **kwargs)
+
+def rlog(*args, **kwargs):
+    print(*args, **kwargs)
+
