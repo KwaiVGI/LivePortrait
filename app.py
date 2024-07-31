d3a54a7d9502c9276842351caa9654a3f11f09bf
zhangdingyun <zhangdingyun@kuaishou.com>
2024-07-31 21:28:33 +0800

    feat: update

--- app.py
+++ app.py
@@ -314,6 +314,7 @@ with gr.Blocks(theme=gr.themes.Soft(font=[gr.themes.GoogleFont("Plus Jakarta San
                         [osp.join(example_portrait_dir, "s13.mp4")],
                         [osp.join(example_portrait_dir, "s18.mp4")],
                         [osp.join(example_portrait_dir, "s20.mp4")],
+                        [osp.join(example_portrait_dir, "s31.mp4")],
                         [osp.join(example_portrait_dir, "s55.mp4")],
                     ],
                     inputs=[retargeting_input_video],
