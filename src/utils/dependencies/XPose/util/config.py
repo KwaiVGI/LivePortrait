e4754e8abb24362ffa217ffc6add9ebb0bef6a9f
刘悦 <zcxey2911@hotmail.com>
2024-08-03 20:59:24 +0800

    Close temporary file handle
    Close temporary file handle

--- src/utils/dependencies/XPose/util/config.py
+++ src/utils/dependencies/XPose/util/config.py
@@ -81,6 +81,7 @@ class Config(object):
                 temp_config_file = tempfile.NamedTemporaryFile(
                     dir=temp_config_dir, suffix='.py')
                 temp_config_name = osp.basename(temp_config_file.name)
+                temp_config_file.close()
                 shutil.copyfile(filename,
                                 osp.join(temp_config_dir, temp_config_name))
                 temp_module_name = osp.splitext(temp_config_name)[0]
