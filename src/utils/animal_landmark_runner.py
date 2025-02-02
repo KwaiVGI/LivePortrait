32edc3784194d233fb47ba0157d92cef0673f0c6
guojianzhu <guojianzhu@kuaishou.com>
2025-02-02 17:03:53 +0800

    fix: issue 483

--- src/utils/animal_landmark_runner.py
+++ src/utils/animal_landmark_runner.py
@@ -43,7 +43,7 @@ class XPoseRunner(object):
         args = Config.fromfile(model_config_path)
         args.device = device
         model = build_model(args)
-        checkpoint = torch.load(model_checkpoint_path, map_location=lambda storage, loc: storage)
+        checkpoint = torch.load(model_checkpoint_path, map_location=lambda storage, loc: storage, weights_only=False)
         load_res = model.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
         model.eval()
         return model
